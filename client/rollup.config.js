6b751129a8007d6ead3f4bfe26f223327ce16f2f
Daniel Lamando <dan@danopia.net>
2020-05-21 15:35:06 +0200

    client: Renew rollup toolchain

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,29 +1,43 @@
-// import resolve from 'rollup-plugin-node-resolve';
-// import commonjs from 'rollup-plugin-commonjs';
-import { terser } from "rollup-plugin-terser";
+import resolve from '@rollup/plugin-node-resolve';
+// import commonjs from '@rollup/plugin-commonjs';
+import ignore from 'rollup-plugin-ignore';
+import { terser } from 'rollup-plugin-terser';
 import pkg from './package.json';
+
+const external = [
+  'ws', 'node-fetch',
+];
 
 export default [
   // browser-friendly UMD build
   {
     input: 'src/main.js',
+    external: ['ws', 'node-fetch'],
     output: {
       name: 'DustClient',
       file: pkg.browser,
       format: 'umd',
       sourcemap: true,
+      globals: {
+        'ws': 'WebSocket',
+        'node-fetch': 'fetch',
+      },
     },
     plugins: [
+      ignore(['path', 'fs/promises']), // used for nodejs filesystem-device.js
       terser(),
-      // resolve(), // so Rollup can find `ms`
-      // commonjs() // so Rollup can convert `ms` to an ES module
+      resolve(), // so Rollup can find `skylink`
+      // commonjs(), // so Rollup can convert things to an ES module
     ],
   },
 
   // CommonJS (for Node) and ES module (for bundlers) build.
   {
     input: 'src/main.js',
-    // external: ['ms'],
+    external: [
+      ...external,
+      '@dustjs/skylink',
+    ],
     output: [
       {
         file: pkg.main,
